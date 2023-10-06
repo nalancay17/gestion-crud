c14ba0bda8be4818f9bcc9057e11435e009d7805
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-10-06 02:27:33 -0300

    Controlador y vista para ver listado de clientes obtenidos de la BBDD.
    Se hace inyección de ClienteDAO.
    
    IMPORTANTE: recordar que, como ClienteDAOImpl usa la annotation Repository, Spring convierte esa clase en un bean y es por ello que puedo inyectarlo en el controlador.

---
+++ src/main/java/com/nico/controlador/ClienteControlador.java
@@ -0,0 +1,27 @@
+package com.nico.controlador;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.nico.dao.ClienteDAO;
+import com.nico.entidad.Cliente;
+
+@Controller
+@RequestMapping("/cliente")
+public class ClienteControlador {
+
+	@Autowired
+	private ClienteDAO clienteDAO;
+
+	@GetMapping("/lista")
+	public String listaClientes(Model modelo) {
+		List<Cliente> clientes = clienteDAO.getClientes();
+		modelo.addAttribute("clientes", clientes);
+		return "lista-clientes";
+	}
+}
