f5c224d015af7515da1d89ba7d194fd18dc640da
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-10-29 00:20:14 -0300

    Bean de AuthenticationManager para autenticación personalizada.
    Retorna un objeto de tipo ProviderManager, que contiene los distintos
    tipos de autenticación soportados, que implementan a
    AuthenticationProvider.
    
    DaoAuthenticationProvider:
    _ Para configuración de autenticación basada en usuario/contraseña.
    _ Los datos se obtienen desde un UserDetailsService (en este caso, el
    que se implementó anteriormente).
    _ En este caso, se usa el bean de PasswordEncoder para que las
    codificaciones las maneje un DelegatingPasswordEncoder.
    
    DelegatingPasswordEncoder:
    _ Implementa la interfaz PasswordEncoder.
    _ Sirve para validar contraseñas codificadas con distintos tipos de
    codificadores.
    _ Permite cambiar la estrategia de almacenamiento de contraseñas en el
    futuro, adaptándola a nuevos algoritmos de hash de contraseñas más
    modernos y seguros SIN REQUERIR que los usuarios restablezcan sus
    contraseñas.

--- src/main/java/com/nico/config/SecurityConfig.java
+++ src/main/java/com/nico/config/SecurityConfig.java
@@ -2,13 +2,14 @@ package com.nico.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -27,19 +28,21 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public UserDetailsService users() {
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		UserDetails user = users
-				.username("user")
-				.password("password")
-				.roles("USER")
-				.build();
-		UserDetails admin = users
-				.username("admin")
-				.password("root")
-				.roles("USER", "ADMIN")
-				.build();
-		return new InMemoryUserDetailsManager(user, admin);
+	public AuthenticationManager authenticationManager() {
+		DaoAuthenticationProvider authenticationProvider = new DaoAuthenticationProvider();
+		authenticationProvider.setUserDetailsService(userDetailsService());
+		authenticationProvider.setPasswordEncoder(passwordEncoder());
+		return new ProviderManager(authenticationProvider);
+	}
+
+	@Bean
+	public UserDetailsService userDetailsService() {
+		return new CustomUserDetailsService();
+	}
+
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return PasswordEncoderFactories.createDelegatingPasswordEncoder();
 	}
 
 }
