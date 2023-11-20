fff8a808939fd555c897ac15abe242dc8773d8e6
Nicolas Alancay <nico.alan.alb@gmail.com>
2023-11-20 15:42:17 -0300

    Vista y controlador para administradores.
    Se agrega el namespace "sec" para agregar las funcionalidades de
    seguridad.
    
    Se agrega un href en la vista de clientes para acceder a la vista de
    administradores. Se usa sec:authorize="hasAuthority('ADMIN') para
    mostrarlo dependiendo si el usuario autenticado tiene la authority
    "ADMIN".

---
+++ src/main/java/com/nico/controlador/AdministradorControlador.java
@@ -0,0 +1,62 @@
+package com.nico.controlador;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import com.nico.dao.ClienteDAO;
+import com.nico.entidad.Cliente;
+
+@Controller
+@RequestMapping("/administrador")
+public class AdministradorControlador {
+
+	@Autowired
+	private ClienteDAO clienteDAO;
+
+	@GetMapping("/lista")
+	public String listaClientes(Model modelo) {
+		List<Cliente> clientes = clienteDAO.getClientes();
+		modelo.addAttribute("clientes", clientes);
+		return "lista-clientes-administrador";
+	}
+
+	@GetMapping("/formularioRegistro")
+	public String formularioRegistro(Model modelo) {
+		modelo.addAttribute("cliente", new Cliente());
+		return "formulario-registro";
+	}
+
+	@PostMapping("/insertarCliente")
+	public String procesarFormularioRegistro(@ModelAttribute("cliente") Cliente cliente) {
+		clienteDAO.insertarCliente(cliente);
+		return "redirect:/administrador/lista";
+	}
+
+	@GetMapping("/formularioActualizacion")
+	public String formularioActualizacion(Model modelo, @RequestParam("id") int id) {
+		Cliente c = clienteDAO.getCliente(id);
+		modelo.addAttribute("cliente", c);
+		return "formulario-actualizacion";
+	}
+
+	@PostMapping("/actualizarCliente")
+	public String procesarFormularioActualizacion(@ModelAttribute("cliente") Cliente cliente) {
+		clienteDAO.actualizarCliente(cliente);
+		return "redirect:/administrador/lista";
+	}
+
+	@GetMapping("/eliminarCliente")
+	public String eliminarCliente(@RequestParam("id") int id) {
+		clienteDAO.eliminarCliente(id);
+		return "redirect:/administrador/lista";
+	}
+
+}
